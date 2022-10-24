6c32946fe2f2dd53aba02f5eb616a71d895fdc47
Sam <104509426+SamM801@users.noreply.github.com>
2022-10-24 09:22:46 +0100

    Delete /target/ due to lack of use

--- target/debug/deps/fgrep-3eb76db90b357c13.d
+++
@@ -1,5 +0,0 @@
-/home/runner/CLI-Program-grep/target/debug/deps/fgrep-3eb76db90b357c13.rmeta: src/main.rs
-
-/home/runner/CLI-Program-grep/target/debug/deps/fgrep-3eb76db90b357c13.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/fgrep-40e017eb635e26f4.d
+++
@@ -1,5 +0,0 @@
-/home/runner/CLI-Program-grep/target/debug/deps/fgrep-40e017eb635e26f4: src/main.rs
-
-/home/runner/CLI-Program-grep/target/debug/deps/fgrep-40e017eb635e26f4.d: src/main.rs
-
-src/main.rs:
